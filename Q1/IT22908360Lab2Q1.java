1b18f7300db3d1b50cdf12509561b2bf9538e59a
Charatha Liyanage <126448686+it22908360@users.noreply.github.com>
2026-09-14 09:01:14 +0530

    Add files via upload

---
+++ Q1/IT22908360Lab2Q1.java
@@ -0,0 +1,12 @@
+public class IT22908360Lab2Q1 {
+    public static void main(String[] args) {
+
+        double perimeter = 100;
+
+        double length = perimeter / 3.5;
+        double width = (3.0 / 4.0) * length;
+
+        System.out.println("Length of the fence: " + length);
+        System.out.println("Width of the fence: " + width);
+    }
+}
