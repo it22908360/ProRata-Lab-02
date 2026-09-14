605a52e0809b9d473dab5fcad305603ba362be67
Charatha Liyanage <126448686+it22908360@users.noreply.github.com>
2026-09-14 09:07:31 +0530

    Add files via upload

---
+++ Q2/IT22908360Lab2Q2.java
@@ -0,0 +1,12 @@
+public class IT22908360Lab2Q2 {
+    public static void main(String[] args) {
+
+        double length = 10;
+        double pi = 3.14;
+
+        double perimeter = 4 * length;
+        double radius = perimeter / (2 * pi);
+
+        System.out.println("Radius of the circular fence: " + radius);
+    }
+}
