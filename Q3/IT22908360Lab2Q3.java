d0faf61995d55549749d91214531fe86f8f5a6d2
Charatha Liyanage <126448686+it22908360@users.noreply.github.com>
2026-09-14 09:09:43 +0530

    Add files via upload

---
+++ Q3/IT22908360Lab2Q3.java
@@ -0,0 +1,11 @@
+public class IT22908360Lab2Q3 {
+    public static void main(String[] args) {
+
+        double sideA = 3;
+        double sideB = 4;
+
+        double hypotenuse = Math.sqrt((sideA * sideA) + (sideB * sideB));
+
+        System.out.println("Length of the hypotenuse: " + hypotenuse);
+    }
+}
